c2036e6924942cedd2db2910311724c8c5c82ed0
Dwengo7 <simonb33r@gmail.com>
2024-05-01 02:39:36 +0100

    push

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Tutor App'
+copyright = '2024, Tutor app team'
+author = 'Tutor app team'
 
 release = '0.1'
 version = '0.1.0'
